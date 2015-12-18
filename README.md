6eb6a6f4568396a8f8be034a0fce3a636df7c485
Evan Hahn <me@evanhahn.com>
2015-12-18 10:21:25 -0800

    Use Standard.js linting

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Hide X-Powered-By
 =================
-
 [![Build Status](https://travis-ci.org/helmetjs/hide-powered-by.svg?branch=master)](https://travis-ci.org/helmetjs/hide-powered-by)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
 
 Simple middleware to remove the `X-Powered-By` HTTP header if it's set.
 
--- test/index.js
+++ test/index.js
@@ -1,64 +1,62 @@
-var hidePoweredBy = require('..');
+var hidePoweredBy = require('..')
 
-var connect = require('connect');
-var request = require('supertest');
-var assert = require('assert');
-
-function helloWorld (req, res) {
-  res.end('Hello world!');
-}
+var connect = require('connect')
+var request = require('supertest')
+var assert = require('assert')
 
 describe('hidePoweredBy', function () {
-
-  function test(name, options) {
+  function test (name, options) {
     it(name, function (done) {
-      var app = connect();
+      var app = connect()
       app.use(function (req, res, next) {
-        res.setHeader('X-Powered-By', 'Computers');
-        next();
-      });
-      app.use(options.middleware);
-      app.use(helloWorld);
+        res.setHeader('X-Powered-By', 'Computers')
+        next()
+      })
+      app.use(options.middleware)
+      app.use(function (req, res) {
+        res.end('Hello world!')
+      })
       request(app).get('/')
-      .end(function (err, res) {
-        if (err) {
-          done(err);
-          return;
-        }
-        assert.equal(res.header['x-powered-by'], options.shouldBe);
-        done();
-      });
-    });
+        .end(function (err, res) {
+          if (err) {
+            done(err)
+            return
+          }
+          assert.equal(res.header['x-powered-by'], options.shouldBe)
+          done()
+        })
+    })
   }
 
   it('works even if no header is set', function (done) {
-    var app = connect();
-    app.use(hidePoweredBy());
-    app.use(helloWorld);
+    var app = connect()
+    app.use(hidePoweredBy())
+    app.use(function (req, res) {
+      res.end('Hello world!')
+    })
     request(app).get('/')
-    .end(function (err, res) {
-      if (err) {
-        done(err);
-        return;
-      }
-      assert.equal(res.header['x-powered-by'], undefined);
-      done();
-    });
-  });
+      .end(function (err, res) {
+        if (err) {
+          done(err)
+          return
+        }
+        assert.equal(res.header['x-powered-by'], undefined)
+        done()
+      })
+  })
 
   test('removes the X-Powered-By header', {
     middleware: hidePoweredBy(),
     shouldBe: undefined
-  });
+  })
 
   test('allows you to explicitly set the header', {
     middleware: hidePoweredBy({ setTo: 'steampowered' }),
     shouldBe: 'steampowered'
-  });
+  })
 
   it('names its function and middleware', function () {
-    assert.equal(hidePoweredBy.name, 'hidePoweredBy');
-    assert.equal(hidePoweredBy().name, 'hidePoweredBy');
-  });
-
-});
+    assert.equal(hidePoweredBy.name, 'hidePoweredBy')
+    assert.equal(hidePoweredBy().name, 'hidePoweredBy')
+  })
+})
